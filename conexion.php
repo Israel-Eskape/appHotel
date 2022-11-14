d58f6ec9b84be280abd35ee56a6ba5d3a1103b57
Israel-Eskape <isra.ixoye@gmail.com>
2022-11-14 12:04:32 -0600

    Se agrega backend con el framework laravel

--- conexion.php
+++ conexion.php
@@ -1,9 +1,10 @@
 <?php
 class Conexion {
-    private $servidor = "localhost";
-    private $usuario = "root";
-    private $contrasena = "77israel77";
-    private $based = "appHotel";
+
+    private $servidor = "162.241.62.132";
+    private $usuario = "efectore_adminapphotel";
+    private $contrasena = "residenciaApphotel2022";
+    private $based = "efectore_apphote";
     private $conn;
 
     public function conectar() {
@@ -11,8 +12,10 @@ class Conexion {
             
         if ($this->conn->connect_errno) {
             echo "Fallo al contenctar a MySQL: (" . $this->conn->connect_errno . ") " . $this->conn->connect_error;
+        }else{
+            echo "Se conectó correctamente la base de datos";#$this->conn->host_info . "\n";
         }
-        echo "Se conectó correctamente la base de datos";#$this->conn->host_info . "\n";
+        
 
     }
 
