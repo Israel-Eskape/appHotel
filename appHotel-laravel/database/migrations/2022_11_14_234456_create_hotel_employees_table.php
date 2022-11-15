2a664e9528aa38dfe6dda93dd9c9a335447278c3
Israel-Eskape <isra.ixoye@gmail.com>
2022-11-14 18:02:40 -0600

    migration table of hotelEmployees

--- appHotel-laravel/database/migrations/2022_11_14_234456_create_hotel_employees_table.php
+++ appHotel-laravel/database/migrations/2022_11_14_234456_create_hotel_employees_table.php
@@ -15,6 +15,22 @@ return new class extends Migration
     {
         Schema::create('hotelEmployees', function (Blueprint $table) {
             $table->id();
+            $table->string('rfc',13);
+            $table->string('password');
+            $table->unsignedBigInteger('hotelPeople_id');
+            $table->unsignedBigInteger('hotelStatusEntity_id');
+            $table->foreign('hotelPeople_id')
+                            ->references('id')
+                            ->on('hotelPeoples')
+                            ->onUpdate('cascade')
+                            ->onDelete('cascade');
+
+            $table->foreign('hotelStatusEntity_id')
+                            ->references('id')
+                            ->on('hotelStatusEntitys')
+                            ->onUpdate('cascade')
+                            ->onDelete('cascade');
+            
             $table->timestamps();
         });
     }
