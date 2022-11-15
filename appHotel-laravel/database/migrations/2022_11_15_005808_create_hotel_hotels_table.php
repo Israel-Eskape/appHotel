34cdcba955d4fbe27ace606ccb170b66a49940fe
Israel-Eskape <isra.ixoye@gmail.com>
2022-11-14 19:29:40 -0600

    migration hotelHotels

--- appHotel-laravel/database/migrations/2022_11_15_005808_create_hotel_hotels_table.php
+++ appHotel-laravel/database/migrations/2022_11_15_005808_create_hotel_hotels_table.php
@@ -15,6 +15,36 @@ return new class extends Migration
     {
         Schema::create('hotelHotels', function (Blueprint $table) {
             $table->id();
+            $table->string('name',50);
+            $table->text('description');
+            $table->string('addres',100);
+            $table->string('phone',20);
+            $table->string('email',50);
+            $table->unsignedBigInteger('hotelIddtec_id');
+            $table->unsignedBigInteger('hotelEmployee_id');
+            $table->unsignedBigInteger('hotelRoom_id');
+            $table->unsignedBigInteger('hotelStatusEntity_id');
+            $table->foreign('hotelIddtec_id')
+                            ->references('id')
+                            ->on('hotelIddtecs')
+                            ->onUpdate('cascade')
+                            ->onDelete('cascade');
+            $table->foreign('hotelEmployee_id')
+                            ->references('id')
+                            ->on('hotelEmployees')
+                            ->onUpdate('cascade')
+                            ->onDelete('cascade');
+            $table->foreign('hotelRoom_id')
+                            ->references('id')
+                            ->on('hotelRooms')
+                            ->onUpdate('cascade')
+                            ->onDelete('cascade');   
+            $table->foreign('hotelStatusEntity_id')
+                            ->references('id')
+                            ->on('hotelStatusEntitys')
+                            ->onUpdate('cascade')
+                            ->onDelete('cascade');
+            
             $table->timestamps();
         });
     }
